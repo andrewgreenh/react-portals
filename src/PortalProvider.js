1e4aea8de97925ac2534483360888a317f979312
Andreas Roth <art_github@rtline.de>
2017-06-15 11:06:14 +0200

    Fixes broken import

--- src/PortalProvider.js
+++ src/PortalProvider.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import withContext from 'recompose';
+import withContext from 'recompose/withContext';
 import PortalConnector from './PortalConnector';
 
 export const portalConnector = new PortalConnector();
